b9d2146a2f0bd8f1adbe670799f8d6f78918397f
sujalshrestha10 <suzolshrestha@gmail.com>
2025-02-17 20:49:05 +0545

    hello

---
+++ tictac/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import Board from './Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic-Tac-Toe</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
